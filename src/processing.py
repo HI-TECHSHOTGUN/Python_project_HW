da13155c9c889eddfad7d6c702910fabb3ca698a
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2024-12-15 23:39:52 +0300

    new func in processing.py and docstring

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,18 @@
 from typing import Union
 
 
-def filter_by_state(data: Union[list], state='EXECUTED') ->list:
+def filter_by_state(x: Union[list], state: str = "EXECUTED") -> list:
+    """Функция для сортировки по состоянию операций"""
     filtered_1 = []
-    for item in data:
-        if item.get('state') == state:
+    for item in x:
+        if item.get("state") == state:
             filtered_1.append(item)
 
     return filtered_1
 
 
+def sort_by_date(data: Union[list], reverse: bool = True) -> list:
+    """Функция для сортировки даты по убыванию"""
+    sorted_data = sorted(data, key=lambda item: tuple(map(int, item["date"][:10].split("-"))), reverse=reverse)
+
+    return sorted_data
