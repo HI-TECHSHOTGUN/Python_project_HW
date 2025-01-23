74f537d888ad0cdbcc709c33d13eb05f1f1ae5c7
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-01-23 20:13:41 +0300

    attempt to do homework

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import json
+
 from src.external_api import loader_apilayer
-from src.generators import filter_by_currency, transact, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transact
+from src.processing import filter_by_state, sort_by_date
 from src.utils import transactions_from_json
 from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state, sort_by_date
-import json
+
+log_file_masks = "logs/masks.log"
+log_file_utils = "logs/utils.log"
+with open(log_file_masks, "w"):
+    pass
+with open(log_file_utils, "w"):
+    pass
 
 
 number_card_or_account = input("Сюда карту|счет: ")
@@ -35,18 +43,18 @@ except StopIteration:
     print("Конец")
 
 try:
-    start_input = int(input('Нижний регистр диапазона'))
-    stop_input = int(input('Верхний регистр диапазона'))
+    start_input = int(input("Нижний регистр диапазона"))
+    stop_input = int(input("Верхний регистр диапазона"))
     for card_number in card_number_generator(start_input, stop_input):
         print(card_number)
 except ValueError:
     print("Ошибка")
 
-print(transactions_from_json('E:/Desktop/Python_Prj/PythonProject1/data/operations.json'))
+print(transactions_from_json("E:/Desktop/Python_Prj/PythonProject1/data/operations.json"))
 
-with open('E:/Desktop/Python_Prj/PythonProject1/data/operations.json', 'r', encoding='utf-8') as f:
+with open("E:/Desktop/Python_Prj/PythonProject1/data/operations.json", "r", encoding="utf-8") as f:
     data = json.load(f)
-    amount_input = (data[1]['operationAmount']['amount'])
-    valet_input = (data[1]['operationAmount']['currency']['code'])
+    amount_input = data[1]["operationAmount"]["amount"]
+    valet_input = data[1]["operationAmount"]["currency"]["code"]
 
     print(loader_apilayer(amount_input, valet_input))
--- src/utils.py
+++ src/utils.py
@@ -1,50 +1,49 @@
 import json
-import os
 import logging
+import os
 
-
-logger = logging.getLogger('utils')
+logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/utils.log', encoding="utf-8")
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def transactions_from_json(file)-> list:
+def transactions_from_json(file) -> list:
     """Считывает данные из .json файла"""
-    logger.info(f'Проверка входящих данных по типу')
+    logger.info("Проверка входящих данных по типу")
     if not isinstance(file, str):
         logger.error("Ошибка: Путь к файлу должен быть строкой")
         return []
 
-    logger.info(f'Поиск файла в проекте')
+    logger.info("Поиск файла в проекте")
     if not os.path.exists(file):
         logger.error(f"Ошибка: файл {file} не найден")
         return []
 
-    logger.info(f'Проверка файла на содержание данных')
+    logger.info("Проверка файла на содержание данных")
     if os.stat(file).st_size == 0:
         logger.error(f"Ошибка: файл {file} пустой.")
         return []
     try:
         with open(file, "r", encoding="utf-8") as f:
-            logger.info(f'Запись данных в переменную')
+            logger.info("Запись данных в переменную")
             try:
                 data = json.load(f)
-                logger.info(f'Успех, запись произведена')
+                logger.info("Успех, запись произведена")
 
             except json.JSONDecodeError:
-                logger.error(f'Ошибка')
+                logger.error("Ошибка")
                 return []
 
-        logger.info(f'Проверка переменной на соотношения по типу')
+        logger.info("Проверка переменной на соотношения по типу")
         if isinstance(data, list):
-            logger.info(f'Успех, возврат данных')
+            logger.info("Успех, возврат данных")
             return data
 
         else:
-            logger.error(f'Ошибка проверки')
+            logger.error("Ошибка проверки")
             return []
 
     except Exception as e:
