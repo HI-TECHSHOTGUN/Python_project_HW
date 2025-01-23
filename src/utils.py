7d3e71299b3bea3663a8acd03763fabcb1cc7654
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-01-23 19:51:19 +0300

    logger in utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,37 +4,49 @@ import logging
 
 
 logger = logging.getLogger('utils')
-file_handler = logging.FileHandler('')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log', encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def transactions_from_json(file)-> list:
+    """Считывает данные из .json файла"""
+    logger.info(f'Проверка входящих данных по типу')
     if not isinstance(file, str):
-        print("Ошибка: Путь к файлу должен быть строкой")
+        logger.error("Ошибка: Путь к файлу должен быть строкой")
         return []
 
+    logger.info(f'Поиск файла в проекте')
     if not os.path.exists(file):
-        print(f"Ошибка: файл {file} не найден")
+        logger.error(f"Ошибка: файл {file} не найден")
         return []
 
+    logger.info(f'Проверка файла на содержание данных')
     if os.stat(file).st_size == 0:
-        print(f"Ошибка: файл {file} пустой.")
+        logger.error(f"Ошибка: файл {file} пустой.")
         return []
     try:
         with open(file, "r", encoding="utf-8") as f:
+            logger.info(f'Запись данных в переменную')
             try:
                 data = json.load(f)
+                logger.info(f'Успех, запись произведена')
 
             except json.JSONDecodeError:
-                print("Ошибка: Некорректный JSON")
+                logger.error(f'Ошибка')
                 return []
 
+        logger.info(f'Проверка переменной на соотношения по типу')
         if isinstance(data, list):
+            logger.info(f'Успех, возврат данных')
             return data
 
         else:
-            print("Ошибка: JSON файл должен содержать список")
+            logger.error(f'Ошибка проверки')
             return []
 
     except Exception as e:
-        print(f"Непредвиденная ошибка: {e}")
+        logger.error(f"Непредвиденная ошибка: {e}")
         return []
