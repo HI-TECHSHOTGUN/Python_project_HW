6c2bdfbdc4afb6fe9c8d6559809d6a1416ec5938
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-01-27 00:30:52 +0300

    fixes and add new module

---
+++ src/module_pd.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def read_csv(file_name_csv):
+    try:
+        csv_dict = pd.read_csv(file_name_csv)
+        csv_list = csv_dict.to_dict(orient='records')
+
+        return csv_list
+
+    except Exception as e:
+        print(f'Ошибка {e}')
+        return []
+
+
+def read_xlsx(file_name_excel):
+    try:
+        xlsx_dict = pd.read_excel(file_name_excel)
+        xlsx_list = xlsx_dict.to_dict(orient='records')
+
+        return xlsx_list
+
+    except Exception as e:
+        print(f'Ошибка {e}')
+        return []
+
+
