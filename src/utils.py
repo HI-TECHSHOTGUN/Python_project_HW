5212dd31e11fbfd713526fa1b9aee7b61c40219a
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-01-27 11:54:32 +0300

    fixes

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,11 @@ import json
 import logging
 import os
 
+MASTER_DIR = os.path.dirname(os.path.dirname(__file__))
+
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8")
+file_handler = logging.FileHandler(os.path.join(MASTER_DIR, 'logs', "utils.log"), encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
