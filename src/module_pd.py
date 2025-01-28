583a6077e7dfee54dc313268ba7c8c12c95d23eb
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-01-28 12:31:23 +0300

    fixes

--- src/module_pd.py
+++ src/module_pd.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def read_csv(file_name_csv):
+    """Функция для чтения файлов с расширением .csv"""
     try:
         csv_dict = pd.read_csv(file_name_csv)
         csv_list = csv_dict.to_dict(orient="records")
@@ -14,6 +15,7 @@ def read_csv(file_name_csv):
 
 
 def read_xlsx(file_name_excel):
+    """Функция для чтения файлов с расширением .xlsx"""
     try:
         xlsx_dict = pd.read_excel(file_name_excel)
         xlsx_list = xlsx_dict.to_dict(orient="records")
